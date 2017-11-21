fd41f0bf7369c5241faf610fa21e9deaac18dec3
Alex K <the3mp3@gmail.com>
2017-11-22 00:43:12 +0200

    update json

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(function(request, response) {
     console.log("In comes a request to: " + request.url);
     response.end("Hello, world!");
 });
-http.createServer(app).listen(8000);
+http.createServer(app).listen(3000);
